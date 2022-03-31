618a7e6acafb22e3e5053b62dd14e7a6231dabe0
James-OP <82921171+James-OP@users.noreply.github.com>
2022-03-30 21:20:13 -0500

    updated javascript

--- atHome.js
+++ atHome.js
@@ -1,7 +1,7 @@
 
 
 //$('.go').on('click',function(){
-function randomLine(){
+/*function randomLine(){
     var file = "atHome.txt";
     const fs = require('fs');
     fs.readFile(file, "utf-8", function(err, data){
@@ -14,5 +14,20 @@ function randomLine(){
         document.getElementById("firstIdea").innerHTML = line;
         console.log(line);
     })
+    
+}
+*/
+
+function randomLine(){
+    const lines1 = ["Do a cartwheel","Do a pushup", "Perform Shakespeare", "Reenact a movie scene", "Jump up and down"];
+    var line1=lines1[Math.floor(Math.random() * lines.length)];
+
+    document.getElementById("firstIdea").innerHTML = line1;
+    console.log(line1);
+    
+    const lines2 = ["eating a spoon full of sugar", "calculating the area of the room", "balancing on one foot"];
+    var line2 = lines2[Math.floor(Math.random() * lines.length)];
+
+    document.getElementById("secondIdea").innerHTML = line2;
     
 }
