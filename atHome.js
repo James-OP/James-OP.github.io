eba11e824f10616b9cadf8a98fdb18a06b2f65eb
James-OP <82921171+James-OP@users.noreply.github.com>
2022-03-30 21:31:48 -0500

    Update atHome.js

--- atHome.js
+++ atHome.js
@@ -19,13 +19,13 @@
 */
 
 function randomLine(){
-    const lines1 = ["Do a cartwheel","Do a pushup", "Perform Shakespeare", "Reenact a movie scene", "Jump up and down"];
+    var lines1 = ["Do a cartwheel","Do a pushup", "Perform Shakespeare", "Reenact a movie scene", "Jump up and down"];
     var line1=lines1[Math.floor(Math.random() * lines1.length)];
 
     document.getElementById("firstIdea").innerHTML = line1;
     console.log(line1);
     
-    const lines2 = ["eating a spoon full of sugar", "calculating the area of the room", "balancing on one foot"];
+    var lines2 = ["eating a spoon full of sugar", "calculating the area of the room", "balancing on one foot"];
     var line2 = lines2[Math.floor(Math.random() * lines2.length)];
 
     document.getElementById("secondIdea").innerHTML = line2;
