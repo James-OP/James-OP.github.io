405f6fc92648753ac50c05eb287a1a98bb406d86
James-OP <82921171+James-OP@users.noreply.github.com>
2022-04-01 17:30:38 -0500

    Adding Wisconsin Ideas

--- travel.js
+++ travel.js
@@ -1,22 +1,26 @@
-/*
+
 function randomLine(){
-    var lines1 = ["Do a cartwheel","Do a pushup", "Perform Shakespeare", "Reenact a movie scene", "Jump up and down"];
-    var line1=lines1[Math.floor(Math.random() * lines1.length)];
+    var selection = document.getElementById('stateSelect');
+    if(selection.value == "49"){
+        var lines1 = ["House on the rock","Dr Evermore's Forevertron", "Devils Lake", "Lake Michigan", "Lake Superior", "the Mississippi River", "Lake Geneva", "Door County"];
+        var line1=lines1[Math.floor(Math.random() * lines1.length)];
 
-    document.getElementById("firstIdea").innerHTML = line1;
-    console.log(line1);
+        document.getElementById("firstIdea").innerHTML = "Go to " + line1;
+        console.log(line1);
     
-    var lines2 = ["eating a spoon full of sugar", "calculating the area of the room", "balancing on one foot"];
-    var line2 = lines2[Math.floor(Math.random() * lines2.length)];
+        var lines2 = ["take a selfie", "leave Aidan behind", "do 10 pushups", "read a book"];
+        var line2 = lines2[Math.floor(Math.random() * lines2.length)];
 
-    document.getElementById("secondIdea").innerHTML = line2;
+        document.getElementById("secondIdea").innerHTML = "and " + line2;
 
-    var lines3 = ["eating a spoon full of sugar", "calculating the area of the room", "balancing on one foot"];
-    var line3 = lines3[Math.floor(Math.random() * lines3.length)];
+        var lines3 = ["eating a spoon full of sugar", "calculating the area of the room", "balancing on one foot"];
+        var line3 = lines3[Math.floor(Math.random() * lines3.length)];
 
-    document.getElementById("thirdIdea").innerHTML = line3;
+        document.getElementById("thirdIdea").innerHTML = "while " + line3;
+    }
+    
 }
-*/
+
 
 function surpriseMe(){
     var selection = document.getElementById('stateSelect');
